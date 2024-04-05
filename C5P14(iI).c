6ae7b70367cd2472516976174ecf78964af6c4c4
Sultan <sultanhazratghani1@gmail.com>
2024-04-05 20:52:31 +0500

    Add files via upload

---
+++ C5P14(iI).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#define p(n) printf("%.2f\n",n);
+//#define p(n) printf("%s",n);
+int main(){
+	float num1 = 27.25;
+	float num2;
+	char name[]="sultan";
+	num2 = 1.0 / 3.0;
+	p(num1);
+	p(num2);
+//	p(name);
+	return 0;
+}
