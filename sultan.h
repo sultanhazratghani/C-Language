191006b752a94139e9f60ee1b1b5b83456d073d2
Sultan <sultanhazratghani1@gmail.com>
2024-04-05 20:52:51 +0500

    Add files via upload

---
+++ sultan.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define pi 3.142
+#define areaCircle(radius) (pi * radius *radius)
+#define areaSquar(length, width) (length * length)
+#define areaRectanlge(length, width) (length * width)
+#define areaTraingle(base, height) (base * height / 2)
+#define areaEllipse(radius1, radius2) (pi * radius1 * radius2)
+#define areaTrapezoin(height, side1, side2) (height * (side1 + side2) / 2)
